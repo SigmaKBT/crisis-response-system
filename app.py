f4eba9791d5bce7a8f4a76dba27446793411e6c7
SigmaKBT <156655148+SigmaKBT@users.noreply.github.com>
2026-04-28 22:26:56 +0530

    deployment fix

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO
 import sqlite3
+import os
+
+port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=port)
 
 app = Flask(__name__)
 app.secret_key = "secret"
